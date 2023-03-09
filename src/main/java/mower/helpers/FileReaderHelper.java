24d1cb48ba7a04ac6da771356c5ceb08c1a5091c
Soumaila DIAKITE <soum.diakite@outlook.com>
2023-03-09 11:53:41 +0100

    Test to get file content

--- src/main/java/mower/helpers/FileReaderHelper.java
+++ src/main/java/mower/helpers/FileReaderHelper.java
@@ -1,9 +1,11 @@
 package mower.helpers;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 public class FileReaderHelper {
-    public static String readFile(String filePath) throws FileNotFoundException {
+    public static List<String> readFile(String filePath) throws IOException {
         throw new FileNotFoundException("File not found");
     }
 }
--- src/test/java/mower/helpers/FileReaderHelperTest.java
+++ src/test/java/mower/helpers/FileReaderHelperTest.java
@@ -3,11 +3,13 @@ package mower.helpers;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.*;
 
-public class FileReaderHeperTest {
+public class FileReaderHelperTest {
     @Test
     public void shouldReturnExceptionWhenFileNotFound() {
         // when
@@ -17,5 +19,14 @@ public class FileReaderHeperTest {
         String expectedMessage = "File not found";
         String actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void shouldReturnFileContentAsString() throws IOException {
+        // When
+        List<String> result = FileReaderHelper.readFile("src/test/resources/data/data.txt");
+
+        // Then
+        assertEquals(result, asList("5 5", "1 2 N", "GAGAGAGAA", "3 3 E", "AADAADADDA"));
     }
 }
