61fa9b493cf84238ce8502313af7530daee6ac81
Soumaila DIAKITE <soum.diakite@outlook.com>
2023-03-09 11:54:21 +0100

    Fix testing to get file content

--- src/main/java/mower/helpers/FileReaderHelper.java
+++ src/main/java/mower/helpers/FileReaderHelper.java
@@ -1,11 +1,22 @@
 package mower.helpers;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileReaderHelper {
     public static List<String> readFile(String filePath) throws IOException {
-        throw new FileNotFoundException("File not found");
+        List<String> result = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            reader.lines().forEach(result::add);
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("File not found");
+        }
+        return result;
     }
 }
