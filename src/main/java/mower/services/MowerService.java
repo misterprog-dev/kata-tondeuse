47141c9c4ba3731e91ecf23994c1145f46eedf87
Soumaila DIAKITE <soum.diakite@outlook.com>
2023-03-09 15:19:33 +0100

    Refactoring : using position helper

--- src/main/java/mower/services/MowerService.java
+++ src/main/java/mower/services/MowerService.java
@@ -3,7 +3,10 @@ package mower.services;
 import mower.exception.FileFormatInvalidException;
 import mower.exception.InvalidGardenSizeException;
 import mower.exception.MowerInitialPositionException;
-import mower.models.*;
+import mower.models.Command;
+import mower.models.LimitGarden;
+import mower.models.Mower;
+import mower.models.Position;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,12 +14,10 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-import static java.lang.Integer.parseInt;
-import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 import static mower.helpers.FileReaderHelper.readFile;
 import static mower.helpers.GardenHelper.getLimitGarden;
-import static org.apache.commons.lang3.StringUtils.isNumeric;
+import static mower.helpers.PositionHelper.getMowerPosition;
 
 public class MowerService {
     private final List<Mower> mowers = new ArrayList<>();
@@ -33,26 +34,12 @@ public class MowerService {
 
     private void constructMowers(Iterator<String> fileLinesIterator, LimitGarden limitGarden) throws MowerInitialPositionException {
         while(fileLinesIterator.hasNext()) {
-            Position mowerPosition = getPosition(fileLinesIterator.next());
+            Position mowerPosition = getMowerPosition(fileLinesIterator.next());
             List<Command> mowerCommands = getCommands(fileLinesIterator.next()).stream()
                     .map(Command::fromCode)
                     .collect(toList());
             mowers.add(new Mower(limitGarden, mowerPosition, mowerCommands));
         }
-    }
-
-    private Position getPosition(String mowerPosition) throws MowerInitialPositionException {
-        String[] splitOfMowerPosition = mowerPosition.split(" ");
-
-        if (isNotInitialPositionValidForMower(splitOfMowerPosition)) {
-            throw new MowerInitialPositionException("Invalid position for Mower");
-        }
-
-        return new Position(parseInt(splitOfMowerPosition[0]), parseInt(splitOfMowerPosition[1]), Direction.fromCode(splitOfMowerPosition[2]));
-    }
-
-    private boolean isNotInitialPositionValidForMower(String[] line) {
-        return stream(line).count() < 3 || !isNumeric(line[0]) || !isNumeric(line[1]);
     }
 
     private List<String> getCommands(String mowerControlLine) {
