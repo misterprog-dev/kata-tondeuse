d0f872d310215959dfc98809eaf5239c8403572b
Soumaila DIAKITE <soum.diakite@outlook.com>
2023-03-09 17:29:30 +0100

    Read file from main

--- src/main/java/mower/Application.java
+++ src/main/java/mower/Application.java
@@ -1,8 +1,19 @@
 package mower;
 
+import mower.exception.FileFormatInvalidException;
+import mower.exception.InvalidGardenSizeException;
+import mower.exception.MowerInitialPositionException;
+import mower.models.Position;
+import mower.services.MowerService;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Application {
 
-    public static void main(final String[] args) {
-        // For app
+    public static void main(final String[] args) throws InvalidGardenSizeException, MowerInitialPositionException, IOException, FileFormatInvalidException {
+        MowerService mowerService = new MowerService(args[0]);
+        List<Position> positions = mowerService.launchMowers();
+        System.out.println(mowerService.displayFinalMowerPosition(positions));
     }
 }
