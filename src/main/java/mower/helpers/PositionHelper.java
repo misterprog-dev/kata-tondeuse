9a37c111736f60bfc4608b3f6de7338a84e7ef93
Soumaila DIAKITE <soum.diakite@outlook.com>
2023-03-09 15:06:23 +0100

    Refactoring : Fix testing for exception for invalid mower initial position

--- src/main/java/mower/helpers/PositionHelper.java
+++ src/main/java/mower/helpers/PositionHelper.java
@@ -1,13 +1,26 @@
 package mower.helpers;
 
+import mower.exception.MowerInitialPositionException;
 import mower.models.Direction;
 import mower.models.Position;
 
-import static mower.models.Direction.WEST;
+import static java.lang.Integer.parseInt;
+import static java.util.Arrays.stream;
+import static org.apache.commons.lang3.StringUtils.isNumeric;
 
 public class PositionHelper {
 
-    public static Position getMowerPosition(String position) {
-        return new Position(0, 0, WEST);
+    public static Position getMowerPosition(String position) throws MowerInitialPositionException {
+        String[] splitOfMowerPosition = position.split(" ");
+
+        if (isNotInitialPositionValidForMower(splitOfMowerPosition)) {
+            throw new MowerInitialPositionException("Invalid position for Mower");
+        }
+
+        return new Position(parseInt(splitOfMowerPosition[0]), parseInt(splitOfMowerPosition[1]), Direction.fromCode(splitOfMowerPosition[2]));
+    }
+
+    static boolean isNotInitialPositionValidForMower(String[] line) {
+        return stream(line).count() < 3 || !isNumeric(line[0]) || !isNumeric(line[1]);
     }
 }
