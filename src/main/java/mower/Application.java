8ad8d99a63041e4a5d129e0ac872cff26a11b74d
Soumaila DIAKITE <soum.diakite@outlook.com>
2023-03-10 09:57:29 +0100

    add exception for file not found in main class

--- src/main/java/mower/Application.java
+++ src/main/java/mower/Application.java
@@ -6,12 +6,18 @@ import mower.exception.MowerInitialPositionException;
 import mower.models.Position;
 import mower.services.MowerService;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
 
     public static void main(final String[] args) throws InvalidGardenSizeException, MowerInitialPositionException, IOException, FileFormatInvalidException {
+        if (args == null || args.length < 1) {
+            throw new FileNotFoundException("File Not found");
+        }
+
         MowerService mowerService = new MowerService(args[0]);
         List<Position> positions = mowerService.launchMowers();
         System.out.println(mowerService.displayFinalMowerPosition(positions));
